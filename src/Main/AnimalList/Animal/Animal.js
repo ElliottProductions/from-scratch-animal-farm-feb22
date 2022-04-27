92cfb43573f031b59d2649046d583b9672dcf6c0
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-04-26 20:00:45 -0800

    got the images actually loading

--- src/Main/AnimalList/Animal/Animal.js
+++ src/Main/AnimalList/Animal/Animal.js
@@ -1,6 +1,8 @@
+
+
 export default function Animal(props){
   return (<>
-    <img src={`./AnimalImages/${props.type}.svg`}></img>
+    <img src={`/${props.type}.svg`}></img>
     <p>{props.name}</p>
   </>);
 }
